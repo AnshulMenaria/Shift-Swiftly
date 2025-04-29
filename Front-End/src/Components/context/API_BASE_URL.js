f37e90b25dacfe45545bc72a6408a8b2ef501241
AnshulMenaria <anshulmenaria@gmail.com>
2025-04-29 17:13:28 +0530

    Changes

--- Front-End/src/Components/context/API_BASE_URL.js
+++ Front-End/src/Components/context/API_BASE_URL.js
@@ -1,6 +1,6 @@
-// const API_BASE_URL = 'https://shiftapi.onrender.com/api';
+const API_BASE_URL = 'https://shiftapi.onrender.com/api';
 
-const API_BASE_URL = 'http://localhost:4000/api';
+// const API_BASE_URL = 'http://localhost:4000/api';
 
 // const API_BASE_URL = 'http://13.127.8.198:4000/api';
 
